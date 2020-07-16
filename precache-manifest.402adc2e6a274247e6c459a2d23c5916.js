2961454ab370e4b6106f3700c2e3a3332932d1d0
Kamil Roch <kamilroch12@gmail.com>
2020-07-16 14:18:44 +0200

    Updates

---
+++ precache-manifest.402adc2e6a274247e6c459a2d23c5916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbf24458f01eb90c92c954f4b685471",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "ffed9b6669630300171b",
+    "url": "/todos-list-react/static/css/main.79afd2e8.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffed9b6669630300171b",
+    "url": "/todos-list-react/static/js/main.4f316f45.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
